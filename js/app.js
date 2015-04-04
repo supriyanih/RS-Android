2074b7591d955126547b43e253c2ee2f6f8cb808
SUPRIYANIH <supriyanih@gmail.com>
2015-04-04 18:33:34 +0700

    ionic

--- js/app.js
+++ js/app.js
@@ -21,12 +21,14 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
 
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
+  $ionicConfigProvider.tabs.position("bottom"); //Places them at the bottom for all OS
+$ionicConfigProvider.tabs.style("standard"); //Makes them all look the same across all OS
   $stateProvider
 
   // setup an abstract state for the tabs directive
